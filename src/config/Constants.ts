e60e068e3ed1ddda25b5ff348ea86ad612d05645
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2021-11-15 17:03:52 -0300

    my first and last commit (:

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://adm1:adm1@url-shortener-dio.flrva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
